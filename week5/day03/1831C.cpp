e66de95d0f4d7759b92f8ae620c5e57989c61de6
17ShaRat <mahfuz@duck.com>
2023-07-09 23:27:47 +0600

    Added problems of day 03

---
+++ week5/day03/1831C.cpp
@@ -0,0 +1,139 @@
+/* 
+ * You didn’t come into this world. 
+ * You came out of it, like a wave from the ocean.
+ * You are not a stranger here. 
+				– Alan Watts
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <random>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (a)-1; i >= b; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define nl putchar('\n')
+#define putc(c) putchar(c)
+#define ins insert
+#define sci(x) scanf("%d", &x)
+#define sci2(a,b) scanf("%d %d", &a, &b)
+#define sci3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sci4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define pri(x) printf("%d ", x)
+#define prin(x) printf("%d\n", x)
+#define scl(x) scanf("%lld", &x)
+#define scl2(a,b) scanf("%lld %lld", &a, &b)
+#define scl3(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+#define prl(x) printf("%lld ", x)
+#define prln(x) printf("%lld\n", x)
+#define scs(s) scanf("%s", s)
+#define scs2(s1, s2) scanf("%s %s", s1, s2)
+#define RET return
+const int N = 2e5 + 50;
+vpii g[N];
+vi used(N);
+vi step(N);
+void dfs(int now, int src) {
+	used[src] = 1;
+	trav(p, g[src]) {
+		int v = p.fi;
+		int pos = p.se;
+		if(!used[v]) {
+			step[v] = step[src];
+			if(pos < now) step[v]++;
+			dfs(pos, v);
+		}
+	}
+}
+ll solve() {
+	int n;
+	sci(n);
+	F0R(i, n) {
+		used[i] = 0;
+		step[i] = 0;
+		g[i].clear();
+	}
+	F0R(i, n-1) {
+		int u, v;
+		sci2(u, v);
+		u--, v--;
+		g[u].pb({v,i});
+		g[v].pb({u, i});
+	}
+	step[0] = 1;
+	dfs( g[0].front().se, 0);
+	int mx = 0;
+	F0R(i, n) mx = max(mx, step[i]);
+	RET mx;
+}
+	
+
+
+int main(void) {
+	/* freopen("input.txt", "r", stdin); */
+	/* freopen("output.txt", "w", stdout); */
+		int t;
+		scanf("%d", &t);
+		for (int i = 1; i <= t; i++) {
+			/* printf("Case %d: ", i); */
+	
+
+			
+			printf("%lld\n", (ll) solve());
+	
+			/* if (solve()) { */
+			/* 	printf("YES\n"); */
+			/* } else { */
+			/* 		printf("NO\n"); */
+			/* } */
+		}
+	 
+	RET 0;
+}
+
---
+++ week5/day03/1839C.cpp
@@ -0,0 +1,129 @@
+/* 
+ * You didn’t come into this world. 
+ * You came out of it, like a wave from the ocean.
+ * You are not a stranger here. 
+				– Alan Watts
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <random>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (a)-1; i >= b; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define nl putchar('\n')
+#define putc(c) putchar(c)
+#define ins insert
+#define sci(x) scanf("%d", &x)
+#define sci2(a,b) scanf("%d %d", &a, &b)
+#define sci3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sci4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define pri(x) printf("%d ", x)
+#define prin(x) printf("%d\n", x)
+#define scl(x) scanf("%lld", &x)
+#define scl2(a,b) scanf("%lld %lld", &a, &b)
+#define scl3(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+#define prl(x) printf("%lld ", x)
+#define prln(x) printf("%lld\n", x)
+#define scs(s) scanf("%s", s)
+#define scs2(s1, s2) scanf("%s %s", s1, s2)
+#define RET return
+
+void solve() {
+	int n;
+	sci(n);
+	vi v(n);
+	F0R(i, n) {
+		sci(v[i]);
+	}
+	if(v[n-1] == 1) {
+		puts("NO");
+		RET;
+	}
+	puts("YES");
+	F0Rd(i, n) {
+		if(i > 0 && v[i-1] == 1) {
+			int  cnt = 0;
+			i--;
+			while(i >= 0 && v[i] == 1) {
+				cnt++;
+				i--;
+				printf("0 ");
+			}
+			printf("%d ", cnt);
+			i++;
+		} else {
+			printf("%d ", 0);
+		}
+	}
+	nl;
+}
+int main(void) {
+	/* freopen("input.txt", "r", stdin); */
+	/* freopen("output.txt", "w", stdout); */
+		int t;
+		scanf("%d", &t);
+		for (int i = 1; i <= t; i++) {
+			/* printf("Case %d: ", i); */
+	
+			solve();
+			
+			/* printf("%lld\n", (ll) solve()); */
+	
+			/* if (solve()) { */
+			/* 	printf("YES\n"); */
+			/* } else { */
+			/* 		printf("NO\n"); */
+			/* } */
+		}
+	 
+	RET 0;
+}
+

611455f82aa6b8090206595b6adee6624fd52893
17ShaRat <mahfuz@duck.com>
2023-07-29 23:23:46 +0600

    added 3 of 5 problems of day 02

---
+++ week8/day02/1844C.cpp
@@ -0,0 +1,139 @@
+/* 
+ * You didn’t come into this world. 
+ * You came out of it, like a wave from the ocean.
+ * You are not a stranger here. 
+				– Alan Watts
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <random>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC target("popcnt")
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (a)-1; i >= b; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define nl putchar('\n')
+#define putc(c) putchar(c)
+#define ins insert
+#define sci(x) scanf("%d", &x)
+#define sci2(a,b) scanf("%d %d", &a, &b)
+#define sci3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sci4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define pri(x) printf("%d ", x)
+#define prin(x) printf("%d\n", x)
+#define scl(x) scanf("%lld", &x)
+#define scl2(a,b) scanf("%lld %lld", &a, &b)
+#define scl3(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+#define prl(x) printf("%lld ", x)
+#define prln(x) printf("%lld\n", x)
+#define scs(s) scanf("%s", s)
+#define scs2(s1, s2) scanf("%s %s", s1, s2)
+#define RET return
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll solve() {
+	int n;
+	sci(n);
+	vi a;
+	vi b;
+	ll best = INT64_MIN;
+	F0R(i, n) {
+		int x;
+		sci(x);
+		best = max(best, (ll)x);
+		if(i&1) a.pb(x);
+		else b.pb(x);
+	}
+	sort(all(a));
+	sort(all(b));
+	ll ans1 = 0, ans2 = 0;
+	bool taken = 0;
+	F0R(i, sz(a)) {
+		if(a[i] > 0) {
+			ans1 += a[i];
+			taken = 1;
+		}
+	}
+	if(!taken) ans1 = INT64_MIN;
+	taken = 0;
+	F0R(i, sz(b)) {
+		if(b[i] > 0) {
+			ans2 += b[i];
+			taken = 1;
+		}
+	}
+	if(!taken) ans2 = INT64_MIN;
+	RET max((ll)best, max(ans1, ans2));
+}
+int main(void) {
+	/* freopen("input.txt", "r", stdin); */
+	/* freopen("output.txt", "w", stdout); */
+		int t;
+		scanf("%d", &t);
+		for (int i = 1; i <= t; i++) {
+			/* printf("Case %d: ", i); */
+	
+			/* solve(); */
+			
+			printf("%lld\n", (ll) solve());
+	
+			/* if (solve()) { */
+			/* 	printf("YES\n"); */
+			/* } else { */
+			/* 		printf("NO\n"); */
+			/* } */
+		}
+	 
+	RET 0;
+}
+

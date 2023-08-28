dfd5ea12d3ac35fdf76b941de6ac9e48f0aad25b
17ShaRat <mahfuz@duck.com>
2023-08-28 13:45:22 +0600

    day01 of week12

---
+++ week12/day01/762A.cpp
@@ -0,0 +1,147 @@
+/* 
+ * You didn’t come into this world. 
+ * You came out of it, like a wave from the ocean.
+ * You are not a stranger here. 
+				– Alan Watts
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+#include <iostream>
+#include <list>
+#include <pthread.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <random>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC target("popcnt")
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define uid(a,b) uniform_int_distribution<int>(a,b)(rng)
+#define sz(x) (int)(x).size()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (a)-1; i >= b; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define nl putchar('\n')
+#define putc(c) putchar(c)
+#define ins insert
+#define sci(x) scanf("%d", &x)
+#define sci2(a,b) scanf("%d %d", &a, &b)
+#define sci3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sci4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define pri(x) printf("%d ", x)
+#define prin(x) printf("%d\n", x)
+#define scl(x) scanf("%lld", &x)
+#define scl2(a,b) scanf("%lld %lld", &a, &b)
+#define scl3(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+#define prl(x) printf("%lld ", x)
+#define prln(x) printf("%lld\n", x)
+#define scs(s) scanf("%s", s)
+#define scs2(s1, s2) scanf("%s %s", s1, s2)
+#define RET return
+const int MOD = 1000000007;
+
+
+int main(void) {
+	ll n, k;
+	vl divs;
+	scanf("%lld %lld", &n, &k);
+	ll i = 1;
+	stack<ll> st;
+	while(i*i <= n) {
+		if(n % i == 0) {
+			divs.pb(i);
+			st.push(n/i);
+		}
+		i++;
+	}
+	i--;
+	if(i*i == n) st.pop();
+	while(sz(st)) {
+		divs.pb(st.top());
+		st.pop();
+	}
+	if(sz(divs) < k) {
+		puts("-1");
+		return 0;
+	}
+	dbg(divs);
+	printf("%lld\n", divs[k-1]);
+	 
+	RET 0;
+}
+
